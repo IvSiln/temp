15e7a3ee4957c76b88fbdc7fb8de8e14ea762e30
Ivan <39631093+IvSiln@users.noreply.github.com>
2022-11-11 22:38:28 +0500

    Update tempClass.java
    Кое-что добавил

--- tempClass.java
+++ tempClass.java
@@ -1,6 +1,7 @@
 public class tempClass {
     public static void main(String[] args) {
         System.out.println("Чертовски важное сообщение");
+        System.out.println("Пробуем сделать еще одно изменение");
     }
 
 }
